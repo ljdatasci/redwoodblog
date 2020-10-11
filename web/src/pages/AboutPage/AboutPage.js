b194bbe73394ba8eb458f7109151f347f6d3137c
Laurie Jefferson <ljdatasci@gmail.com>
2020-10-11 12:41:19 -0400

    project files

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,8 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const AboutPage = () => {
+  return <BlogLayout>This is the about page!</BlogLayout>
+}
+
+export default AboutPage
